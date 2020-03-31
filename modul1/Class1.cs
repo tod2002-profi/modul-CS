b6fe06c624924dde3b5f32788a2f6c33c87873f2
tod2002-profi <56667737+tod2002-profi@users.noreply.github.com>
2020-03-31 14:19:10 +0300

    Add files via upload

---
+++ modul1/Class1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul1
+
+{
+    class Matrix
+    {
+        private int rows;
+        private int cols;
+
+        int[,] elements;
+
+        public int Rows
+        {
+            get
+            {
+                return rows;
+            }
+        }
+
+        public int Cols
+        {
+            get
+            {
+                return cols;
+            }
+        }
+
+        public Matrix(int rows, int cols)
+        {
+            this.rows = rows;
+            this.cols = cols;
+
+            elements = new int[rows, cols];
+        }
+
+        public void Input()
+        {
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    Console.Write("Matrix[{0},{1}] = ", i, j);
+                    int value = Convert.ToInt32(Console.ReadLine());
+
+                    elements[i, j] = value;
+                }
+            }
+        }
+
+        public void Output()
+        {
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    Console.Write(elements[i, j]);
+                    Console.Write(" ");
+                }
+
+                Console.WriteLine();
+            }
+        }
+
+        public int FindMaximum()
+        {
+            int max = 0;
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    int el = elements[i, j];
+
+                    if (el > max) max = el;
+                }
+            }
+
+            return max;
+        }
+
+        public int FindMinimum()
+        {
+            int min = elements[0, 0];
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    int el = elements[i, j];
+
+                    if (el < min) min = el;
+                }
+            }
+
+            return min;
+        }
+    }
+}
---
+++ modul1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Matrix m = new Matrix(3, 3);
+
+            m.Input();
+            m.Output();
+
+            Console.WriteLine(m.FindMinimum());
+            Console.WriteLine(m.FindMaximum());
+
+            Console.ReadKey();
+        }
+    }
+}
